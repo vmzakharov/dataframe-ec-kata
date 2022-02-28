0d02a090dabd273563fbf01a8e75c68c9eabdac1
vmzakharov <zakharov.vladimir.m@gmail.com>
2022-02-27 23:23:20 -0500

    work in progress

--- src/test/java/io/github/vmzakharov/ecdataframekata/donutshop/DonutShopTest.java
+++ src/test/java/io/github/vmzakharov/ecdataframekata/donutshop/DonutShopTest.java
@@ -1,7 +1,12 @@
 package io.github.vmzakharov.ecdataframekata.donutshop;
 
 import io.github.vmzakharov.ecdataframe.dataframe.DataFrame;
+import io.github.vmzakharov.ecdataframe.dataframe.DfJoin;
+import org.eclipse.collections.api.factory.Lists;
+import org.eclipse.collections.api.factory.Sets;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.time.Clock;
 import java.time.Instant;
@@ -32,31 +37,80 @@ public class DonutShopTest
 
         this.donutShop.setMenu(
                 new DataFrame("Menu")
-                    .addStringColumn("Code").addStringColumn("Type").addDoubleColumn("Price").addDoubleColumn("Discount Price")
-                    .addRow("BB", "BLUEBERRY", 1.50, 1.25)
+                    .addStringColumn("Code").addStringColumn("Type").addDoubleColumn("Price").addDoubleColumn("DiscountPrice")
+                    .addRow("BB", "BLUEBERRY", 1.75, 1.50)
                     .addRow("GL", "GLAZED", 1.50, 1.25)
                     .addRow("OF", "OLD_FASHIONED", 1.50, 1.25)
-                    .addRow("P",  "PUMPKIN", 1.50, 1.25)
-                    .addRow("J",  "JELLY", 1.50, 1.25)
+                    .addRow("PS", "PUMPKIN_SPICE", 1.75, 1.50)
+                    .addRow("JL",  "JELLY", 1.75, 1.50)
                     .addRow("AC", "APPLE_CIDER", 1.50, 1.25)
         );
 
         this.donutShop.setOrders(
                 new DataFrame("Orders")
-                    .addStringColumn("CustomerId").addDateColumn("Date").addStringColumn("DonutCode").addLongColumn("Count")
+                    .addLongColumn("CustomerId").addDateColumn("Date").addStringColumn("DonutCode").addLongColumn("Count")
                     .addRow(1, this.today, "BB", 2)
-                    .addRow(1, this.today, "BB", 2)
-                    .addRow(1, this.today, "BB", 2)
-                    .addRow(2, this.today, "BB", 2)
-                    .addRow(2, this.today, "BB", 2)
-                    .addRow(3, this.yesterday, "BB", 2)
-                    .addRow(3, this.yesterday, "BB", 2)
-                    .addRow(3, this.tomorrow, "BB", 2)
-                    .addRow(3, this.tomorrow, "BB", 2)
-                    .addRow(4, this.tomorrow, "BB", 2)
-                    .addRow(4, this.tomorrow, "BB", 2)
-                    .addRow(4, this.tomorrow, "BB", 2)
-                    .addRow(4, this.tomorrow, "BB", 2)
+                    .addRow(1, this.today, "GL", 2)
+                    .addRow(1, this.today, "OF", 10)
+
+                    .addRow(2, this.yesterday, "BB", 12)
+                    .addRow(2, this.today, "PS", 12)
+
+                    .addRow(3, this.yesterday, "JL", 2)
+                    .addRow(3, this.tomorrow, "OF", 8)
+                    .addRow(3, this.tomorrow, "BB", 12)
+
+                    .addRow(4, this.yesterday, "PS", 2)
+                    .addRow(4, this.today, "OF", 12)
+                    .addRow(4, this.tomorrow, "OF", 10)
         );
+    }
+
+    @Test
+    public void totalSpendPerCustomer()
+    {
+        this.donutShop.getOrders().lookup(DfJoin
+                    .to(this.donutShop.getMenu())
+                    .match("DonutCode", "Code")
+                    .select(Lists.immutable.of("Price", "DiscountPrice"))
+        );
+
+        this.donutShop.getOrders()
+              .addDoubleColumn("Total", "(Count < 10 ? Price : DiscountPrice)  * Count");
+
+        System.out.println(this.donutShop.getOrders().asCsvString());
+
+        var spendByCustomer =
+                this.donutShop.getOrders().sumBy(Lists.immutable.of("Total"), Lists.immutable.of("CustomerId"));
+
+        System.out.println(spendByCustomer.asCsvString());
+
+        spendByCustomer.sortByExpression("-Total");
+        spendByCustomer.lookup(DfJoin
+                        .to(this.donutShop.getCustomers())
+                        .match("CustomerId", "Id")
+                        .select("Name")
+        );
+
+        System.out.println(spendByCustomer.asCsvString());
+        System.out.println(spendByCustomer.getString("Name", 0));
+
+        var tomorrowsOrders = this.donutShop.getOrders().selectBy("Date == toDate('" + this.tomorrow + "')");
+        System.out.println(tomorrowsOrders.asCsvString());
+        // customer names for delivery tomorrow
+
+        Assertions.assertEquals(
+                Sets.mutable.of("Carol", "Dave"),
+                tomorrowsOrders.lookup(DfJoin
+                    .to(this.donutShop.getCustomers())
+                    .match("CustomerId", "Id")
+                    .select("Name")
+                ).getStringColumn("Name").toList().toSet());
+    }
+
+    @Test
+    public void totalPriceForEachOrder()
+    {
+
     }
 }
---
+++ src/test/java/io/github/vmzakharov/ecdataframekata/util/DataFrameUtil.java
@@ -0,0 +1,39 @@
+package io.github.vmzakharov.ecdataframekata.util;
+
+import io.github.vmzakharov.ecdataframe.dataframe.DataFrame;
+import io.github.vmzakharov.ecdataframe.dataframe.DfColumn;
+import org.junit.jupiter.api.Assertions;
+
+final public class DataFrameUtil
+{
+    private DataFrameUtil()
+    {
+        // Utility class
+    }
+
+    static public void assertEquals(DataFrame expected, DataFrame actual)
+    {
+
+        Assertions.assertTrue(
+                expected.rowCount() != actual.rowCount() || expected.columnCount() != actual.columnCount(),
+                "Dimensions don't match: expected rows " + expected.rowCount() + ", cols " + expected.columnCount()
+                    + ", actual rows " + actual.rowCount() + ", cols " + actual.columnCount());
+
+        Assertions.assertEquals(
+                expected.getColumns().collect(DfColumn::getName), actual.getColumns().collect(DfColumn::getName),
+                "Column names mismatch");
+
+        int colCount = expected.columnCount();
+        int rowCount = expected.rowCount();
+
+        for (int rowIndex = 0; rowIndex < rowCount; rowIndex++)
+        {
+            for (int colIndex = 0; colIndex < colCount; colIndex++)
+            {
+                Assertions.assertEquals(
+                        expected.getObject(rowIndex, colIndex), actual.getObject(rowIndex, colIndex),
+                        "Different values in row " + rowIndex + ", column " + colIndex);
+            }
+        }
+    }
+}
